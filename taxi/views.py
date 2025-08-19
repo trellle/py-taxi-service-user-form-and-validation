77caedc1d40bf4460ed8e54ec8e788ce57e5183a
yarik <super.racing@ukr.net>
2025-08-19 09:15:05 +0300

    created views for Driver

--- taxi/views.py
+++ taxi/views.py
@@ -87,3 +87,18 @@ class DriverListView(LoginRequiredMixin, generic.ListView):
 class DriverDetailView(LoginRequiredMixin, generic.DetailView):
     model = Driver
     queryset = Driver.objects.all().prefetch_related("cars__manufacturer")
+
+
+class DriverCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Driver
+    fields = ["username", "license_number", "password", "first_name", "last_name", "email"]
+
+
+class DriverUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Driver
+    fields = ["username", "license_number", "password", "first_name", "last_name", "email"]
+
+
+class DriverDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Driver
+    fields = ["license_number"]
